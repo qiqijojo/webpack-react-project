673a0ceacd300e74c994283d2e0a913f095fdd05
王齐 qi.wang01 <qi.wang01@weimob.com>
2019-03-06 11:33:22 +0800

    配置webpack输入输出

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+// import React from 'react';
+// import ReactDom from 'react-dom';
 
-class Demo extends React.Component {
-    render() {
-        return (
-            <div>hello react</div>
-        );
-    }
-}
-ReactDOM.render(
-    <Demo />,
-    document.getElementById('root')
-);
+// class Demo extends React.Component {
+//     render() {
+//         return (
+//             <div>hello react</div>
+//         );
+//     }
+// }
+// ReactDOM.render(
+//     <Demo />,
+//     document.getElementById('root')
+// );
+window.document.getElementById('root').innerText = 'helleeeo-' + 'webpack';
