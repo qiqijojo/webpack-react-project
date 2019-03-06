c9cd27ccf5ab77f4e6c0204b185ba7b34c543734
王齐 qi.wang01 <qi.wang01@weimob.com>
2019-03-06 15:09:10 +0800

    使用devServer：实现实时预览功能、运行环境区分、使用域名运行项目（将public里的index.html引用bundle.js文件改为绝对路径）

--- webpack/webpack.config.development.js
+++ webpack/webpack.config.development.js
@@ -4,5 +4,13 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../dist')
+    },
+    devServer: {
+        hot: true, // 开启热模块替换（必须配合inline一起使用）--实现实时预览
+        inline: true, // 可以单独使用 --实现实时预览
+        host: '0.0.0.0',
+        port: 8080,
+        // historyApiFallback: true,
+        contentBase: path.resolve(__dirname, '../public')
     }
 };
