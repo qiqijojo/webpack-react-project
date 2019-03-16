c407e053118121fe8b34c10326e9f20a2c4c7d14
qiqijojo <774948439@qq.com>
2019-03-16 13:21:39 +0800

    feat: 添加commitlint.config.js配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,24 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    // rules: {
-    //     "type-enum": [2, 'always']
-    // }
+    rules: {
+        'type-case': [2, 'always', 'lower-case'],
+	    'type-empty': [2, 'never'],
+        "type-enum": [
+            2,
+            'always',
+            [
+                'build',
+			    'chore',
+			    'ci',
+			    'docs',
+			    'feat',
+			    'fix',
+			    'perf',
+			    'refactor',
+			    'revert',
+			    'style',
+			    'test'
+            ]
+        ],
+    }
 }
