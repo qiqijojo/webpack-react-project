11b938531c5bfc3b1289cb642e7a2e61de57ebfb
qiqijojo <774948439@qq.com>
2019-03-16 18:36:44 +0800

    chore: 给webpack.config.production.js也进行babel-loader天剑

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -7,6 +7,18 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['@babel/react'] }
+                }],
+                exclude: /node_modules/
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../public/index.html'),
