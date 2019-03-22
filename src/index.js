d23e056c9ce4115067211f5b4400306ce4d1b086
qiqijojo <774948439@qq.com>
2019-03-22 10:37:22 +0800

    chore: 修改lint-staged、对js不走prettier

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const App = () => <div>114啦啦啦11</div>;
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+);
