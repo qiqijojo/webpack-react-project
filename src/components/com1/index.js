455df2a470702cd6f9a8e123378b618930365319
qiqijojo <774948439@qq.com>
2019-05-23 11:44:04 +0800

    chore: 纯函数、class都可支持

--- src/components/com1/index.js
+++ src/components/com1/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import img1 from 'assets/images/aaAa.png';
 import styles from './index.less';
 
-// const Com1 = () => (
-//     <div className={styles['com1-wrapper']}>
-//         <span>com1组件</span>
-//         <img alt="" src={img1} />
-//     </div>
-// );
-class Com1 extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            aa: []
-        };
-    }
+const Com1 = () => (
+    <div className={styles['com1-wrapper']}>
+        <span>com1组件</span>
+        <img alt="" src={img1} />
+    </div>
+);
+// class Com1 extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             aa: []
+//         };
+//     }
 
-    render() {
-        const { aa } = this.state;
-        return (
-            <div className={styles['com1-wrapper']}>
-                <span>com1组件</span>
-                <img alt="" src={img1} />
-            </div>
-        );
-    }
-}
+//     render() {
+//         const { aa } = this.state;
+//         return (
+//             <div className={styles['com1-wrapper']}>
+//                 <span>com1组件</span>
+//                 <img alt="" src={img1} />
+//             </div>
+//         );
+//     }
+// }
 export default Com1;
