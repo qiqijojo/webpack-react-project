7ea39943717accf4e38fc46a9b43d816aeaf362e
qiqijojo <774948439@qq.com>
2019-06-03 19:01:17 +0800

    feat: 用React.Suspense替换react-loadable实现按需加载

--- src/app.js
+++ src/app.js
@@ -1,32 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Suspense } from 'react';
 import { Router, Switch, Route } from 'react-router';
 import { createBrowserHistory } from 'history';
-import Loadable from 'react-loadable';
 import Com1 from 'components/com1/index';
 import '@/app.less';
 
 const historyInstance = createBrowserHistory();
-const ErrorLoading = ({ pastDelay }) => {
-    if (pastDelay) {
-        return <p>loading</p>;
-    }
-    return null;
-};
-const Home = Loadable({
-    loader: () => import('pages/home/index'),
-    loading: ErrorLoading,
-    delay: 200
-});
+const Home = React.lazy(() => import('pages/home/index'));
 const App = () => (
     <Router history={historyInstance}>
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/next' component={Com1} />
-        </Switch>
+        <Suspense fallback={<p>loading</p>}>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/next' component={Com1} />
+            </Switch>
+        </Suspense>
     </Router>
 );
-ErrorLoading.propTypes = {
-    pastDelay: PropTypes.bool.isRequired
-};
 export default App;
