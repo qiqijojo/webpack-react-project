b8fc726481f7c323b0766ca1068385f64431be81
qiqijojo <774948439@qq.com>
2019-06-05 16:03:59 +0800

    fix: 错误边界组件注释掉console

--- src/components/errorBoundary/index.js
+++ src/components/errorBoundary/index.js
@@ -9,21 +9,22 @@ class ErrorBoundary extends Component {
         };
     }
 
-    static getDerivedStateFromError() { // throw new Error抛出会立马捕捉到，并处理
+    static getDerivedStateFromError() {
+        // 接受一个参数error，和底下的didcatch的error一样
+        // throw new Error抛出会立马捕捉到，并处理
         return {
             errorStatus: true
         };
     }
 
-    componentDidCatch(error, info) {
-        // You can also log the error to an error reporting service
-        console.info('8111', error, info);
-    }
+    // componentDidCatch(error, info) {
+    //     // You can also log the error to an error reporting service
+    //     console.info('8111', error, info);
+    // }
 
     render() {
         const { errorStatus } = this.state;
         const { children } = this.props;
-        console.info('ppp', children);
         if (errorStatus) {
             return <div>error</div>;
         }
