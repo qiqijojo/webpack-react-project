08e3f1c6f27a884881e32e8e171e9469e041b16f
qiqijojo <774948439@qq.com>
2019-09-18 14:48:27 +0800

    upgrade: 升级第三方库-minor+patch

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,16 +9,17 @@ module.exports = {
             'always',
             [
                 'build',
-			    'chore',
+			    'chore', // 构建过程或辅助工具的变动
 			    'ci',
-			    'docs',
-			    'feat',
-			    'fix',
+			    'docs', // 文档（documentation）
+			    'feat', // 新功能（feature）
+			    'fix', // 修补bug
 			    'perf',
-			    'refactor',
-			    'revert',
-			    'style',
-			    'test'
+			    'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
+			    'revert', // 回滚
+			    'style', // 格式（不影响代码运行的变动）
+                'test', // 增加测试
+                'upgrade' // 第三方库升级
             ]
         ],
     }
